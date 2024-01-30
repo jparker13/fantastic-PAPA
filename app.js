d2d9fe894e407acfff323fb28395ae7f8bb9bcd0
Janna Beth Parker <jannaparker@macbook-pro-3.main.local.una.edu>
2024-01-30 16:05:00 -0600

    commit static git hub pages stuff

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5500;
+//set the view engine to ejs
+let path = require('path');
+app.set('view engine', 'ejs'); 
+app.set('views' , path.join(__dirname, 'views'));
+
+//use res.render to load up an ejs view file
+
+let myTypeServer = "The peace maker";
 
 app.get('/', function (req, res) {
-    res.send('Hello World from Express \n My name is Janna Parker and I cannot find a fully free ennagram test...\n I have answered atleast 100 questions and they all tried to make me pay before i could see my results \n SO... here are my own answers...\n I will alwasy do my part in the group and then some\n I will call you out for not doing your group work.')
-})
+    res.render('index', {
+        myTypeClient: myTypeServer
+    });
+}); 
 
 //app.listen(3000)
 
