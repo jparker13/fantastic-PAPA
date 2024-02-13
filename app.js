3e5a3f1ae3a41217d8403014dd2b5af1659c1b6a
Janna Beth Parker <jannaparker@MacBook-Pro-3.local>
2024-02-13 17:07:53 -0600

    updates to iss1

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5500;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
 //set the view engine to ejs
 let path = require('path');
 app.set('view engine', 'ejs'); 
@@ -9,6 +13,31 @@ app.set('views' , path.join(__dirname, 'views'));
 //use res.render to load up an ejs view file
 
 let myTypeServer = "I hope i did this right :)";
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', function (req, res) {
     res.render('index', {
