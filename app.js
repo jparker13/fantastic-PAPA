bc48165c264eb19a040dd77cc26cdcce59865a78
Janna Beth Parker <jannaparker@macbook-pro-3.main.local.una.edu>
2024-02-20 15:32:53 -0600

    updated

--- app.js
+++ app.js
@@ -3,17 +3,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5500;
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
-
-//set the view engine to ejs
+const bodyParser = require('body-parser')
+// set the view engine to ejs
 let path = require('path');
-app.set('view engine', 'ejs'); 
-app.set('views' , path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: true }))
 
-//use res.render to load up an ejs view file
+// use res.render to load up an ejs view file
 
-let myTypeServer = "I hope i did this right :)";
-
+let myTypeServer = "9️⃣ The Peacemaker ✌🏻";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.URI, {
@@ -29,6 +28,13 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    const result = await client.db("papa-database").collection("papa-collection").find().toArray();
+
+    //console.log("cxnDB result: ", result);
+
+    return result; 
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
@@ -36,17 +42,44 @@ async function run() {
     await client.close();
   }
 }
+//run().catch(console.dir);
+
+app.get('/read', async (req,res) => {
+
+  let myResultServer = await run(); 
+
+  console.log("myResultServer:", myResultServer[0].userName);
+
+
+
+  res.render('index', {
+    myTypeClient: myTypeServer,
+    myResultClient: myResultServer
+
+  });
+
+
+}); 
 run().catch(console.dir);
 
 
-app.get('/', function (req, res) {
-    res.render('index', {
-        myTypeClient: myTypeServer
-    });
-}); 
 
-//app.listen(3000)
+app.get('/', function(req, res) {
+
+  res.render('index', {
+   
+    myTypeClient: myTypeServer 
+
+  });
+  
+});
+
+
+app.get('/send', function (req, res) {
+  
+    res.send('Hello World from Express <br><a href="/">home</a>')
+})
 
 app.listen(port, () => {
-    console.log(`MIKE app listening on port ${port}`)
+console.log(`papa app listening on port ${port}`)
 })
